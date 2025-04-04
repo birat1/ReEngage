c6014ddd401e8bd9b98a62fdf0285be65b72ca7d
ba01081 <93601583+birat1@users.noreply.github.com>
2025-04-04 10:35:32 +0100

    Changed route for login page to it's own for now

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,7 +2,7 @@ import StarMathGame from "./Games/StarMathGame/StarMath-Game.js";
 import StartGame from "./Games/Fill-itFish/MainGame.js";
 import LabWars from "./Games/LabWars/LabWars.js";
 import Home from "./pages/Home/Home.js";
-import LoginRegister from "./pages/LoginRegister/LoginRegister.jsx";
+import LoginRegister from "./pages/LoginRegister/LoginRegister.js";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -10,7 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
-      <Route path="/" element={<LoginRegister />} />
+      <Route path="/login" element={<LoginRegister />} />
         <Route path="/" element={<Home />} />
         <Route path="/starmath" element={<StarMathGame />} />
         <Route path="/fill-itfish" element={<StartGame />} />
