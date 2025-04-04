6cc816f1b2a1d685954fa01da6a1580980703214
jasongurung <jasongurung24@gmail.com>
2025-04-04 09:54:41 +0100

    Started working on the login page

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -2,6 +2,7 @@ import StarMathGame from "./Games/StarMathGame/StarMath-Game.js";
 import StartGame from "./Games/Fill-itFish/MainGame.js";
 import LabWars from "./Games/LabWars/LabWars.js";
 import Home from "./pages/Home/Home.js";
+import LoginRegister from "./pages/LoginRegister/LoginRegister.jsx";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 
@@ -9,6 +10,7 @@ function App() {
   return (
     <BrowserRouter>
       <Routes>
+      <Route path="/" element={<LoginRegister />} />
         <Route path="/" element={<Home />} />
         <Route path="/starmath" element={<StarMathGame />} />
         <Route path="/fill-itfish" element={<StartGame />} />
