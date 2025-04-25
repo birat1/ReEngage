a9b4ffecc8f6f4060a61867b7aa67a7d716a3b37
Yuken <72272663+AnthemColours@users.noreply.github.com>
2025-04-25 13:05:19 +0100

    Small type error fix in url param

--- django-app/backend/urls.py
+++ django-app/backend/urls.py
@@ -49,8 +49,8 @@ urlpatterns = [
 
     # Routes for Oak OpenAPI
     path('api/oak/units/<str:keystage>/<str:subject>/<int:year>/', UnitsAPI.as_view(), name='oak-units'),
-    path('api/oak/lessons/<int:unit_id>/', LessonsAPI.as_view(), name='oak-lessons'),
+    path('api/oak/lessons/<str:unit>/', LessonsAPI.as_view(), name='oak-lessons'),
     path('api/oak/questions/<str:keystage>/<str:subject>/', QuestionsAPI.as_view(), name='oak-questions'),
-    path('api/oak/lessons/<int:lesson_id>/video/', LessonAssetAPI.as_view(), name='oak-lesson-video'),
-    path('api/oak/lessons/<int:lesson_id>/summary/', LessonSummaryAPI.as_view(), name='oak-lesson-summary'),
+    path('api/oak/lessons/<str:lesson>/video/', LessonAssetAPI.as_view(), name='oak-lesson-video'),
+    path('api/oak/lessons/<str:lesson>/summary/', LessonSummaryAPI.as_view(), name='oak-lesson-summary'),
 ]
