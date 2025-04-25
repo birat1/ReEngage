1c63345605166527094a4af5e193e5043c69ad71
ba01081 <93601583+birat1@users.noreply.github.com>
2025-04-25 16:13:27 +0100

    Installed axios and implemented api from backend

--- react-app/src/Games/LabWars/data/questions.js
+++ react-app/src/Games/LabWars/data/questions.js
@@ -1,7 +1,7 @@
 // TODO: FIGURE OUT HOW TO GET ASSETS FOR SOME QUESTIONS FROM THE API
+import axios from "axios";
 
-const API_URL = "https://open-api.thenational.academy/api/v0/key-stages/{keyStage}/subject/{subject}/questions?offset=0&limit=10"
-const API_KEY = "f38bf624-bdb7-46a7-8bce-b938e5563d0e"
+const API_URL = "http://localhost:8000/api/oak/questions/{keyStage}/{subject}";
 
 // Fetch questions from the National Academy API
 export async function fetchQuestions() {
@@ -10,18 +10,14 @@ export async function fetchQuestions() {
 
   try {
     const url = API_URL.replace("{keyStage}", keyStage).replace("{subject}", subject);
-    const response = await fetch(url, {
+    
+    const response = await axios.get(url, {
       headers: {
-        "Authorization": `Bearer ${API_KEY}`,
         "Content-Type": "application/json",
       },
     });
 
-    if (!response.ok) {
-      throw new Error(`Failed to fetch questions: ${response.status}`);
-    }
-
-    const data = await response.json();
+    const data = response.data;
 
     // Convert array of lessons to an array of questions
     const allQuestions = data.flatMap((lesson) => [
