456cb8ab9d21ebf7aedec140e73569e5882025bc
Yuken <72272663+AnthemColours@users.noreply.github.com>
2025-05-07 21:36:51 +0100

    Home page now displays dashboard when logged in and hero when not logged in

--- react-app/src/pages/Home/Home.js
+++ react-app/src/pages/Home/Home.js
@@ -1,13 +1,26 @@
 import Footer from "../../components/Footer/Footer.js";
 import Hero from "./Hero/Hero.js";
+import Dashboard from "../Dashboard/dashboard.js";
+import { useAuthStatus } from "../../components/Authentication/CheckLoginStatus.js";
 
-function Home () {
+function Home() {
+  const { isLoggedIn } = useAuthStatus();
+
+  if (isLoggedIn) {
     return (
-        <div>
-            <Hero />
-            <Footer/>
-        </div>
+      <>
+        <Dashboard />
+        <Footer />
+      </>
     );
+  } else {
+    return (
+      <>
+        <Hero />
+        <Footer />
+      </>
+    );
+  }
 }
 
 export default Home;
