3e60ef1803c96f7611d54d6cbc1589157c188502
Gianna-A <agianna819@gmail.com>
2025-05-13 00:57:27 +0100

    revised constants.js

--- react-app/src/constants.js
+++ react-app/src/constants.js
@@ -1,1 +1,8 @@
-export const backendAPI = `http://${window.location.hostname}:8000/`;
+let bAPI;
+if (window.location.hostname === 'localhost') {
+    bAPI = `http://${window.location.hostname}:8000/`;
+} else {
+    bAPI = `http://django:8000/`;
+}
+
+export const backendAPI = bAPI;
