11cfc67288fd1e379815058e0528b93de031c9a2
Gianna-A <agianna819@gmail.com>
2025-05-13 10:32:35 +0100

    revised constants logic

--- react-app/src/constants.js
+++ react-app/src/constants.js
@@ -2,7 +2,7 @@ let backAPI
 if (window.location.hostname === 'localhost' || window.location.hostname === '127.0.0.1') {
     backAPI = `http://${window.location.hostname}:8000/`
 } else {
-    backAPI `http://${window.location.hostname}/`
+    backAPI = `http://${window.location.hostname}/`
 }
 
 export const backendAPI = backAPI;
